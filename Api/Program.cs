17c88e208c7479ae3524c9d35a09a9a716002826
owl <oyikavamiko@gmail.com>
2025-07-24 23:38:07 +0300

    Initial Projects

---
+++ Api/Program.cs
@@ -0,0 +1,22 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapCarter();
+
+app.Run();
