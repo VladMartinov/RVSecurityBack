c0a2e2f0541f880009c6934f2632ccd5b151aa84
owl <oyikavamiko@gmail.com>
2025-08-01 14:24:23 +0300

    IUserTokenRepository added

---
+++ Core/Interfaces/Repositories/IUserTokenRepository.cs
@@ -0,0 +1,12 @@
+using Core.Entities;
+
+namespace Core.Interfaces.Repositories;
+
+public interface IUserTokenRepository
+{
+    Task<UserToken?> GetTokenByIdAsync(Guid id, bool track = true, CancellationToken cancellationToken = default);
+    Task<UserToken?> GetTokenByHashAsync(string hash, bool track = true, CancellationToken cancellationToken = default);
+    Task<UserToken> CreateUserTokenAsync(UserToken token, CancellationToken cancellationToken = default);
+    Task<UserToken> UpdateUserTokenAsync(UserToken token, CancellationToken cancellationToken = default);
+    Task DeleteUserTokenAsync(Guid id, CancellationToken cancellationToken = default);
+}
